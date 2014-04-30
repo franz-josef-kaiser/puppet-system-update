580a1edd5d4fb042035104a78953c4186e1b556b
Kaiser Franz Josef <wecodemore@gmail.com>
2014-04-30 12:34:14 +0200

    Added autoclean and fix broken apt-get tasks.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,37 +1,49 @@
 class systemupdate {
-  # Update catalogue
+  # @license CC-BY-SA 3.0
+  # @link http://askubuntu.com/q/3167/
+  # Autoclean not longer available packages
+  exec { 'apt-autoclean':
+    command => '/usr/bin/apt-get autoclean'
+  }
+  # Update package catalogue
   exec { 'apt-get-update':
-    command => '/usr/bin/apt-get update'
+    command => '/usr/bin/apt-get update',
+    require => Exec['apt-autoclean'],
+  }
+  # Fix broken packages
+  exec { 'apt-fix-broken':
+    command => '/usr/bin/apt-get -f install',
+    require => Exec['apt-autoclean'],
   }
 
   if ! defined( Package['curl'] ) {
     package { 'curl':
       ensure  => 'present',
-      require => Exec['apt-get-update'],
+      require => Exec['apt-autoclean'],
     }
   }
   if ! defined( Package['yum'] ) {
     package { 'yum':
       ensure  => 'present',
-      require => Exec['apt-get-update'],
+      require => Exec['apt-autoclean'],
     }
   }
   if ! defined( Package['rpm'] ) {
     package { 'rpm':
       ensure  => 'present',
-      require => Exec['apt-get-update'],
+      require => Exec['apt-autoclean'],
     }
   }
   if ! defined( Package['make'] ) {
     package { 'make':
       ensure  => 'present',
-      require => Exec['apt-get-update'],
+      require => Exec['apt-autoclean'],
     }
   }
   if ! defined( Package['chkconfig'] ) {
     package { 'chkconfig':
       ensure  => 'present',
-      require => Exec['apt-get-update'],
+      require => Exec['apt-autoclean'],
     }
   }
 }
